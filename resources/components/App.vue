78f4aa95435ad278a80a70c099ae1d313c6dca6e
BecanGaming0 <df531830@gmail.com>
2024-09-11 08:51:38 -0600

    Fomukario

--- resources/components/App.vue
+++ resources/components/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div>        
         Hola desde App de Vue
+        <a href="/login">Login</a>
     </div>
 </template>
 <script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,8 @@
 import './bootstrap';
 
 import { createApp } from 'vue';
-import App from '../components/App.vue';
+import App from '../components/Formulario.vue';
+import '../css/Formulario.css' // Importa el archivo CSS global
 
 const app = createApp(App);
 app.mount('#app')
